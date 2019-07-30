c7bb644f4297671e585b604fd9d68fe5a85650a8
Andrea N <andreain3d@gmail.com>
2019-07-29 21:04:33 -0400

    added cli table display of all items from sql table

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,6 @@
 var inquirer = require("inquirer");
 var mysql = require("mysql");
+var Table = require("cli-table");
 
 var connection = mysql.createConnection({
   host: "localhost",
@@ -16,7 +17,24 @@ connection.connect(function(err) {
   if (err) throw err;
   connection.query("SELECT * FROM products", function(err, res) {
     if (err) throw err;
-    console.log(res);
+    var productsTable = new Table({
+      head: ["ID", "Name", "Department", "Price", "# Available"],
+      colWidths: [10, 20, 20, 10, 15]
+    });
+
+    for (i = 0; i < res.length; i++) {
+      var itemData = [];
+      itemData.push(
+        res[i].item_id,
+        res[i].product_name,
+        res[i].department_name,
+        res[i].price,
+        res[i].stock_quantity
+      );
+      productsTable.push(itemData);
+    }
+
+    console.log(productsTable.toString());
 
     connection.end();
   });
